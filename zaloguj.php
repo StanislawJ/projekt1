d74073e2d6701b12b37cbf72b49d4630ed506fea
StanislawJ <stacho1011@op.pl>
2017-11-08 20:33:02 +0100

    już ok

---
+++ zaloguj.php
@@ -0,0 +1,49 @@
+<?php
+
+
+	session_start();
+
+	require_once "connect.php";
+	$connecting = @new mysqli($host, $db_user, $db_password, $db_name);
+
+
+	if($connecting->connect_errno!=0)
+	{
+		//echo "error<br/>".$connecting->connect_errno;
+	}
+	else
+	{
+		//echo "działa<br/>";
+	}
+
+
+
+	if(isset($_SESSION['log']))
+	{
+
+
+	}
+	else
+	{
+
+	$log = $_POST['login'];
+	$pass = $_POST['pass'];
+
+	$sql = "SELECT * FROM `users` WHERE login LIKE '$log' AND pass LIKE '$pass'";
+	$rezultat = $connecting->query($sql);
+
+
+	if($rezultat->num_rows > 0)
+	{
+
+		$table = $rezultat->fetch_array();
+		$_SESSION['username'] = $table['login'];
+		$_SESSION['log'] = 1;
+
+	}
+	else echo "błędne dane logowania";
+
+
+	}
+
+?>
