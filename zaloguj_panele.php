549129a17287c22de4cb811ed989b2f08122cf80
StanislawJ <stacho1011@op.pl>
2017-11-08 14:58:20 +0100

    z modulem

--- zaloguj_panele.php
+++ zaloguj_panele.php
@@ -1,15 +1,15 @@
-<?php session_start(); 
+<?php session_start();
 if(isset($_SESSION['log']))
 {
-} 
-else 
-{ 
+}
+else
+{ ?>
 	<div class="navbar navbar-inverse" id="log_nav">
       <div class="panel-heading">
         <h4 class="panel-title">
-		
-				<div id="menu">	
-				<div id="logo">AUKCJONER</div> 
+
+				<div id="menu">
+				<div id="logo">AUKCJONER</div>
 				<button type="button" class="button2 button" href="#collapse1" data-toggle="collapse">zaloguj</button>
 				<button type="button" class="button2 button" href="#collapse2" data-toggle="collapse">zarejestruj</button>
 				</div>
@@ -17,7 +17,7 @@ else
       </div>
       <div id="collapse1" class="panel-collapse collapse">
         <ul class="list-group">
-          <li class="list-group-item"> 
+          <li class="list-group-item">
 			<div id="log">
 			<form >
 				login: <input type="text" name="log"  id="log_name" autofocus/>
@@ -29,5 +29,6 @@ else
         </ul>
       </div>
     </div>
-} 
+		<?php
+}
 ?>
