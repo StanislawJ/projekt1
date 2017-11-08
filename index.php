2fb925eaf72787eb0b6f7f8768b1676ac55d9e00
StanislawJ <stacho1011@op.pl>
2017-11-08 14:46:21 +0100

    dale

--- index.php
+++ index.php
@@ -1,49 +1,34 @@
 <!DOCTYPE HTML>
 <html lang="pl">
 <head>
-
-	<link rel="shortcut icon" href="favicon.ico" type="image/x-icon">
-
 	<meta charset="utf-8"/>
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 	<link href="css/bootstrap.min.css" rel="stylesheet">
 
-		<link rel="stylesheet" type="text/css" href="style.css" media="all">
-	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
-
-	
+	<link rel="stylesheet" type="text/css" href="style.css" media="all">
 	<script language="JavaScript" src="gen_validatorv4.js" type="text/javascript" xml:space="preserve"></script>
-	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
- 
-	
+	<script type="text/javascript" src="jquery.js"></script>
 	<title>Aukcjoner</title>
 	
-	<?php
-	session_start();
-	if(isset($_SESSION['log']))
-	{
-		header("Location: aukcje.php");
-	}
-	else
-	{
-		echo 'nie zalogowany';
-	}
-	?>
+	
 </head>
 <body>
 
 
 
 <div class="container">
-    <div class="navbar navbar-inverse">
+
+<?php session_start(); if(isset($_SESSION['log'])){ ?>
+<?php } else { ?>
+	<div class="navbar navbar-inverse" id="log_nav">
       <div class="panel-heading">
         <h4 class="panel-title">
 		
 				<div id="menu">	
 				<div id="logo">AUKCJONER</div> 
 				<button type="button" class="button2 button" href="#collapse1" data-toggle="collapse">zaloguj</button>
-				<button type="button" class="button2 button" data-toggle="modal" data-target="#myModal"> rejestracja</button>
+				<button type="button" class="button2 button" href="#collapse2" data-toggle="collapse">zarejestruj</button>
 				</div>
 		</h4>
       </div>
@@ -52,7 +37,7 @@
           <li class="list-group-item"> 
 			<div id="log">
 			<form >
-				login: <input type="text" name="log" id="log_name"/>
+				login: <input type="text" name="log"  id="log_name" autofocus/>
 				hasło: <input type="password" name="pass" id="log_pass"/>
 				<input type="submit" id="logi"/>
 			</form>
@@ -61,21 +46,21 @@
         </ul>
       </div>
     </div>
- 
+<?php } ?>
 
- 
 
- <div class="modal fade" id="myModal" role="dialog">
-    <div class="modal-dialog">
-    
-      <!-- Modal content-->
-      <div class="modal-content">
-        <div class="modal-header">
-          <button type="button" class="close" data-dismiss="modal">&times;</button>
-          
-        </div>
-        <div class="modal-body">    
-      
+
+
+	<div id="content" >
+		<div id="reg">
+		<div class="panel-heading">
+        
+		
+				<div id="menu">	
+				</div>
+		</h4>
+      </div>
+      <div id="collapse2" class="panel-collapse collapse">
         <ul  class="list-group">
           <li id="ground" class="list-group-item"> 
 			<div id="log">
@@ -111,7 +96,7 @@
 				</li>
 				
 				<li class="list-group-item">
-					miejscowość: <input type="text" name="city"/>
+					miejscowość: <input type="text" name="from"/>
 				</li>
 				
 				<li class="list-group-item">
@@ -134,23 +119,19 @@
   var frmvalidator  = new Validator("myform");
 
   frmvalidator.EnableMsgsTogether();
-  frmvalidator.addValidation("log_reg", "req", "Prosze podac login");
-  frmvalidator.addValidation("log_reg", "minlen=6",	"Minimalna dlugosc loginu to 6 znakow");
-  frmvalidator.addValidation("log_reg", "maxlen=20", "Maksymalna dlugosc loginu to 20 znakow");
-  frmvalidator.addValidation("log_reg", "alphanumeric", "Niepoprawny typ znakow w loginie");
+  frmvalidator.addValidation("log_reg","req","Prosze podac login");
+  frmvalidator.addValidation("log_reg","maxlen=20",	"Maksymalna dlugosc to 20 znakow");
+  frmvalidator.addValidation("log_reg","alphanumeric","Niepoprawny typ znakow w loginie");
   
   frmvalidator.addValidation("pass_reg","req","Prosze podac haslo");
-  frmvalidator.addValidation("pass_reg","minlen=6",	"Minimalna dlugosc hasla to 6 znakow");
-  frmvalidator.addValidation("pass_reg","maxlen=20","Maksymalna dlugosc hasla to 20 znakow ");
+  frmvalidator.addValidation("pass_reg","maxlen=20","Maksymalna dlugosc to 20 znakow hasła");
   
-  frmvalidator.addValidation("pass_reg_repeat","req","Prosze pwtorzyc haslo");
-  frmvalidator.addValidation("pass_reg_repeat","minlen=6",	"Minimalna dlugosc hasla to 6 znakow");
-  frmvalidator.addValidation("pass_reg_repeat","maxlen=20","Maksymalna dlugosc hasla to 20 znakow ");
-
+  frmvalidator.addValidation("pass_reg_repeat","req","Prosze powtorzyc haslo");
+  frmvalidator.addValidation("pass_reg_repeat","maxlen=20","Maksymalna dlugosc to 20 znakow hasła");
   
   frmvalidator.addValidation("name","req","Prosze podac imie");
   frmvalidator.addValidation("name","maxlen=30",	"Maksymalna dlugosc imienia to 30 znakow");
-  frmvalidator.addValidation("name","alpha","Niepoprawny typ znakow w imieniu");
+  frmvalidator.addValidation("name","alpha","Niepoprawny typ znakow imienia");
   
   frmvalidator.addValidation("lastname","req","Prosze podac nazwisko");
   frmvalidator.addValidation("lastname","maxlen=30",	"Maksymalna dlugosc nazwiska to 30 znakow");
@@ -160,42 +141,46 @@
   frmvalidator.addValidation("email_reg","req", "Prosze podac email");
   frmvalidator.addValidation("email_reg","email" , "Niepoprawny typ znakow email'a");
   
-  frmvalidator.addValidation("city","req","Prosze podac miasto");
-  frmvalidator.addValidation("city","maxlen=30", "Maksymalna dlugosc nazwy miasta to 30 znakow");
-  frmvalidator.addValidation("city","alphanumeric_space", "Niepoprawny typ znakow w nazwie mieście");
+  frmvalidator.addValidation("from","req","Prosze podac miasto");
+  frmvalidator.addValidation("from","maxlen=30",	"Maksymalna dlugosc miasta to 30 znakow");
+  frmvalidator.addValidation("from","alpha","Niepoprawny typ znakow w mieście");
   
   frmvalidator.addValidation("street","req","Prosze podac ulice");
   frmvalidator.addValidation("street","maxlen=30",	"Maksymalna dlugosc nazwy ulicy to 30 znakow");
-  frmvalidator.addValidation("street","alpha","Niepoprawny typ znakow w nazwie ulicy");
+  frmvalidator.addValidation("street","alpha","Niepoprawny typ znakow nazwy ulicy");
   
   frmvalidator.addValidation("home_nr","req","Prosze podac nr domu");
   frmvalidator.addValidation("home_nr","maxlen=10");
-  frmvalidator.addValidation("home_nr","alphanumeric_space","Niepoprawny typ znakow w nr domu");
+  frmvalidator.addValidation("log_reg","alphanumeric","Niepoprawny typ znakow w loginie");
  
-  frmvalidator.addValidation("phone_nr","req","Prosze podac nr telefonu");
+  frmvalidator.addValidation("phone_nr","req","Prosze podac nr domu");
   frmvalidator.addValidation("phone_nr","maxlen=9");
-  frmvalidator.addValidation("phone_nr","numeric", "Niepoprawny typ znakow w nr telefonu");
+  frmvalidator.addValidation("phone_nr","numeric", "Niepoprawny typ znakow telefnu");
   
 
 //]]></script>
 
+
+
+
+
 			</div>
 		  </li>
         </ul>
-		
 		</div>
-        <div class="modal-footer">
-          <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
-        </div>
-      </div>
-      
-    </div>
-  </div>
-
-
-	<div id="content" >
-	
+		</div>
 	</div>
+	
+
+
+
+
+
+
+
+
+
+
 
 </div>
 <script>
