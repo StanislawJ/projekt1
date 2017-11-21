2343b215a127869e94b51f68812f4d07eb672004
bartek9801 <bartek980123@interia.pl>
2017-11-21 20:52:34 +0100

    23

--- aukcjoner.sql
+++ aukcjoner.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 4.7.4
+-- version 4.7.0
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Czas generowania: 30 Paź 2017, 21:22
+-- Czas generowania: 21 Lis 2017, 20:50
 -- Wersja serwera: 10.1.26-MariaDB
--- Wersja PHP: 7.0.22
+-- Wersja PHP: 7.1.8
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET AUTOCOMMIT = 0;
@@ -30,10 +30,8 @@ SET time_zone = "+00:00";
 
 CREATE TABLE `auction` (
   `ID_AUK` int(20) NOT NULL,
-  `nazwa` varchar(20) COLLATE utf8_polish_ci NOT NULL,
   `ID_KUP` int(20) NOT NULL,
   `ID_SPRZ` int(20) NOT NULL,
-  `ID_ZDJ` int(15) NOT NULL,
   `typ` varchar(20) COLLATE utf8_polish_ci NOT NULL,
   `kategoria` varchar(20) COLLATE utf8_polish_ci NOT NULL,
   `data_zacz` date NOT NULL,
@@ -42,6 +40,15 @@ CREATE TABLE `auction` (
   `cena` decimal(10,2) NOT NULL,
   `cena_pocz` decimal(10,2) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_polish_ci;
+
+--
+-- Zrzut danych tabeli `auction`
+--
+
+INSERT INTO `auction` (`ID_AUK`, `ID_KUP`, `ID_SPRZ`, `typ`, `kategoria`, `data_zacz`, `kr_op`, `dl_op`, `cena`, `cena_pocz`) VALUES
+(5, 1, 2, 'klasyczna', 'laptopy', '2017-11-15', 'asdasdasdaa sdasdasdaasd asdasdaasdasdasdaa sdasdasdaasdasdasdaas dasdasdaasdasdasda asdasdasdaasdas dasdaasdasdasdaasdasda sdaasdasdasdaasdasdasdaaaa', 'asdfasdfasdfasfasfasfasfasfasdfasdf00000000000000000000000000000000000000000000000000', '5000.00', '0.00'),
+(6, 0, 2, 'klasyczna', 'motory', '0000-00-00', 'niema', 'ma dużo ', '10000.00', '0.00'),
+(7, 0, 2, 'licytacja', 'laptopy', '0000-00-00', 'asdf', 'asdfasdfasdfa', '59959.00', '0.00');
 
 -- --------------------------------------------------------
 
@@ -53,21 +60,34 @@ CREATE TABLE `categories` (
   `kategoria` varchar(20) COLLATE utf8_polish_ci NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_polish_ci;
 
+--
+-- Zrzut danych tabeli `categories`
+--
+
+INSERT INTO `categories` (`kategoria`) VALUES
+('elektronika'),
+('motoryzacja');
+
 -- --------------------------------------------------------
 
 --
--- Struktura tabeli dla tabeli `photos`
+-- Struktura tabeli dla tabeli `categories_2`
 --
 
-CREATE TABLE `photos` (
-  `ID_ZDJ` int(15) NOT NULL,
-  `zd_1` varchar(20) COLLATE utf8_polish_ci NOT NULL,
-  `zd_2` varchar(20) COLLATE utf8_polish_ci NOT NULL,
-  `zd_3` varchar(20) COLLATE utf8_polish_ci NOT NULL,
-  `zd_4` varchar(20) COLLATE utf8_polish_ci NOT NULL,
-  `zd_5` varchar(20) COLLATE utf8_polish_ci NOT NULL,
-  `zd_6` varchar(20) COLLATE utf8_polish_ci NOT NULL
+CREATE TABLE `categories_2` (
+  `pod_kategoria` varchar(20) COLLATE utf8_polish_ci NOT NULL,
+  `kategoria` varchar(20) COLLATE utf8_polish_ci NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_polish_ci;
+
+--
+-- Zrzut danych tabeli `categories_2`
+--
+
+INSERT INTO `categories_2` (`pod_kategoria`, `kategoria`) VALUES
+('laptopy', 'elektronika'),
+('stacjonarne', 'elektronika'),
+('motory', 'motoryzacja'),
+('samochody', 'motoryzacja');
 
 -- --------------------------------------------------------
 
@@ -78,6 +98,15 @@ CREATE TABLE `photos` (
 CREATE TABLE `typy` (
   `typ` varchar(20) COLLATE utf8_polish_ci NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_polish_ci;
+
+--
+-- Zrzut danych tabeli `typy`
+--
+
+INSERT INTO `typy` (`typ`) VALUES
+('holenderska'),
+('klasyczna'),
+('licytacja');
 
 -- --------------------------------------------------------
 
@@ -99,6 +128,14 @@ CREATE TABLE `users` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_polish_ci;
 
 --
+-- Zrzut danych tabeli `users`
+--
+
+INSERT INTO `users` (`ID`, `login`, `pass`, `name`, `lastname`, `email`, `city`, `street`, `home`, `phone`) VALUES
+(1, 'admin', 'admin', '', '', '', '', '', '', 0),
+(2, 'bartosz123', 'bartek123', 'bartosz', 'borkowski', 'bor222@op.pl', 'siedlce', 'długa', 'k/54', 123456789);
+
+--
 -- Indeksy dla zrzutów tabel
 --
 
@@ -109,7 +146,6 @@ ALTER TABLE `auction`
   ADD PRIMARY KEY (`ID_AUK`),
   ADD KEY `kategoria_fk` (`kategoria`),
   ADD KEY `typ_fk` (`typ`),
-  ADD KEY `ID_ZDJ_fk` (`ID_ZDJ`),
   ADD KEY `ID_SPRZ_fk` (`ID_SPRZ`),
   ADD KEY `ID_KUP_fk` (`ID_KUP`);
 
@@ -120,10 +156,11 @@ ALTER TABLE `categories`
   ADD PRIMARY KEY (`kategoria`);
 
 --
--- Indexes for table `photos`
+-- Indexes for table `categories_2`
 --
-ALTER TABLE `photos`
-  ADD PRIMARY KEY (`ID_ZDJ`);
+ALTER TABLE `categories_2`
+  ADD PRIMARY KEY (`pod_kategoria`),
+  ADD KEY `categories_2_ibfk_1` (`kategoria`);
 
 --
 -- Indexes for table `typy`
@@ -145,20 +182,12 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT dla tabeli `auction`
 --
 ALTER TABLE `auction`
-  MODIFY `ID_AUK` int(20) NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT dla tabeli `photos`
---
-ALTER TABLE `photos`
-  MODIFY `ID_ZDJ` int(15) NOT NULL AUTO_INCREMENT;
-
+  MODIFY `ID_AUK` int(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
 --
 -- AUTO_INCREMENT dla tabeli `users`
 --
 ALTER TABLE `users`
-  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT;
-
+  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 --
 -- Ograniczenia dla zrzutów tabel
 --
@@ -167,11 +196,14 @@ ALTER TABLE `users`
 -- Ograniczenia dla tabeli `auction`
 --
 ALTER TABLE `auction`
-  ADD CONSTRAINT `ID_KUP_fk` FOREIGN KEY (`ID_KUP`) REFERENCES `users` (`ID`) ON UPDATE CASCADE,
-  ADD CONSTRAINT `ID_SPRZ_fk` FOREIGN KEY (`ID_SPRZ`) REFERENCES `users` (`ID`) ON UPDATE CASCADE,
-  ADD CONSTRAINT `ID_ZDJ_fk` FOREIGN KEY (`ID_ZDJ`) REFERENCES `photos` (`ID_ZDJ`) ON UPDATE CASCADE,
-  ADD CONSTRAINT `kategoria_fk` FOREIGN KEY (`kategoria`) REFERENCES `categories` (`kategoria`) ON UPDATE CASCADE,
+  ADD CONSTRAINT `kategoria_fk` FOREIGN KEY (`kategoria`) REFERENCES `categories_2` (`pod_kategoria`) ON UPDATE CASCADE,
   ADD CONSTRAINT `typ_fk` FOREIGN KEY (`typ`) REFERENCES `typy` (`typ`) ON UPDATE CASCADE;
+
+--
+-- Ograniczenia dla tabeli `categories_2`
+--
+ALTER TABLE `categories_2`
+  ADD CONSTRAINT `categories_2_ibfk_1` FOREIGN KEY (`kategoria`) REFERENCES `categories` (`kategoria`) ON UPDATE CASCADE;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
