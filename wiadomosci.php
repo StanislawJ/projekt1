69f657ade784b7e201400e3471d2e4e728d500f1
StanislawJ <stacho1011@op.pl>
2017-12-08 21:05:45 +0100

    mesage XDXD

--- wiadomosci.php
+++ wiadomosci.php
@@ -1,42 +1,43 @@
-<?php
-session_start();
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+    <meta charset="utf-8">
+  	<title></title>
 
-require_once "connect.php";
-$connecting = @new mysqli($host, $db_user, $db_password, $db_name);
+  <!ikona przy nazwie strony>
+  	<link rel="shortcut icon" href="favicon.ico" type="image/x-icon">
 
-$sql = "SELECT * FROM `users` WHERE ID LIKE '".$_SESSION['user_id']."'";
-$rezult = $connecting->query($sql);
-$infoK = mysqli_fetch_assoc($rezult);
-
-$sql = "SELECT * FROM `message` WHERE login_kup LIKE '".$infoK['login']."'";
-$rezult = $connecting->query($sql);
-$quantity = $rezult->num_rows;
-
-
-
+  	<meta charset="utf-8"/>
+  	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
 
 
-
-for($k=0;$tab = mysqli_fetch_assoc($rezult);$k++)
-    {
-
-        echo"<div class='item' id='".$tab['ID_MESS']."'>";
-        echo"<div id='od'>".$tab['login_sprz']."</div>";
-        echo"<div id='temat'></div>";
-        echo"<div id='data'>wysłano - ".$tab['data_wysl']."</div>";
-        echo"</div>";
-    }
+  	<link rel="stylesheet" type="text/css" href="style.css" media="all">
+  	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
 
 
+  	<script language="JavaScript" src="gen_validatorv4.js" type="text/javascript" xml:space="preserve"></script>
+  	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
 
 
+  </head>
+  <body>
+
+<div id='panel_wiad'>
+  <?php require_once('wiadomosci_panel.php') ?>
+</div>
+
+<div id='text_wiad'>
+  <div id='text_WW'>
+    <?php require_once('lista_wiad.php') ?>
+  </div>
+</div>
+
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+<script src="js/bootstrap.min.js"></script>
+</body>
 
 
-
-
-
-
-
-
-
-?>
+</html>
